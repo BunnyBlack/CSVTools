c8bf9d1909334153a6d1dade2b99d81b49dbc042
BunnyBlack <xyy6836@126.com>
2021-05-29 21:52:45 +0800

    所有Excel 转 CSV

--- CSVTools/Assets/Editor/CsvTool.cs
+++ CSVTools/Assets/Editor/CsvTool.cs
@@ -1,9 +1,57 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
 
 namespace Editor
 {
-    public class CsvTool : UnityEditor.Editor
+    public static class CsvTool
     {
-        
+        public static string CsvToolName = @"csvtool.py";
+
+        [MenuItem("打表工具/所有Excel转CSV")]
+        public static void ExcelToCsvAll()
+        {
+            DoExcelToCsvAll();
+        }
+
+        [MenuItem("打表工具/单个Excel转CSV")]
+        public static void ExcelToCsv()
+        {
+
+        }
+
+        private static void DoExcelToCsvAll()
+        {
+            var cmdCommand =
+                $"\"{Application.dataPath}/config/excel2csv/{CsvToolName}\" \"excel_to_csv_all('{Application.dataPath}')\"";
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    //设置要启动的应用程序
+                    FileName = "C:\\Users\\86187\\AppData\\Local\\Programs\\Python\\Python39\\python.exe",
+                    // 是否使用操作系统shell启动
+                    UseShellExecute = false,
+                    // 接受来自调用程序的输入信息
+                    RedirectStandardInput = true,
+                    // 输出信息
+                    RedirectStandardOutput = true,
+                    // 输出错误
+                    RedirectStandardError = true,
+                    // 不显示程序窗口
+                    CreateNoWindow = false,
+                    Arguments = cmdCommand
+                }
+            };
+            //启动程序
+            p.Start();
+            var outputInfo = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+
+            Debug.Log(outputInfo);
+            Debug.Log("所有Excel转CSV");
+        }
     }
 }
--- tools/python/csvtool.py
+++ tools/python/csvtool.py
@@ -1,10 +1,12 @@
 import os
+import sys
+
 import pandas
 
 
 def excel_to_csv_all(config_path: str):
-    xlsx_path = config_path + "\\xlsx"
-    csv_path = config_path + "\\csv"
+    xlsx_path = config_path + "/config/xlsx"
+    csv_path = config_path + "/config/csv"
     xlsx_list = []
     file_list = os.listdir(xlsx_path)
     for filename in file_list:
@@ -12,14 +14,20 @@ def excel_to_csv_all(config_path: str):
             xlsx_list.append(filename)
     for filename in xlsx_list:
         csv_name = filename.split("-")[0] + ".csv"
-        xlsx_file = pandas.read_excel(filename)
-        xlsx_file.to_csv(csv_path + csv_name, encoding="GB2312")
+        xlsx_file = pandas.read_excel(f"{xlsx_path}/{filename}")
+        xlsx_file.to_csv(f"{csv_path}/{csv_name}", encoding="GB2312")
+        print(f"Generate: {csv_path}/{csv_name}")
+    print("Complete")
 
 
 def excel_to_csv(config_path: str, xlsx_name: str):
-    xlsx_path = config_path + "\\xlsx"
-    csv_path = config_path + "\\csv"
+    xlsx_path = config_path + "/config/xlsx"
+    csv_path = config_path + "/config/csv"
     csv_name = xlsx_name.split("-")[0] + ".csv"
-    xlsx_file = pandas.read_excel(xlsx_path + xlsx_name)
-    xlsx_file.to_csv(csv_path + csv_name, encoding="GB2312")
+    xlsx_file = pandas.read_excel(f"{xlsx_path}/{xlsx_name}")
+    xlsx_file.to_csv(f"{csv_path}/{csv_name}", encoding="GB2312")
+    print(f"Generate: {csv_path}/{csv_name}")
 
+
+if __name__ == '__main__':
+    eval(sys.argv[1])
